bbd6f2d07da87c2d9f23013078d4cea0e704652d
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-09-22 04:38:39 +0530

    feat: added bet fn

---
+++ programs/solana_fpl/src/instructions/bet.rs
@@ -0,0 +1,40 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Token, TokenAccount, Transfer};
+
+use crate::state::EscrowAccount;
+
+#[derive(Accounts)]
+pub struct Bet<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(mut)]
+    pub user_token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub escrow_token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub escrow_account: Account<'info, EscrowAccount>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+}
+
+pub fn handler_bet<'info>(ctx: Context<Bet>) -> Result<()> {
+    let escrow_account = &mut ctx.accounts.escrow_account;
+    let bet_amount = escrow_account.bet_amount;
+    transfer_to_escrow(ctx.accounts, bet_amount)?;
+    Ok(())
+}
+
+fn transfer_to_escrow<'info>(accounts: &mut Bet<'info>, amount: u64) -> Result<()> {
+    let cpi_accounts = Transfer {
+        from: accounts.user_token_account.to_account_info(),
+        to: accounts.escrow_token_account.to_account_info(),
+        authority: accounts.user.to_account_info(),
+    };
+    let cpi_program = accounts.token_program.to_account_info();
+    let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+    token::transfer(cpi_ctx, amount)?;
+    let escrow_account = &mut accounts.escrow_account;
+    escrow_account.usdc_balance += amount as u128;
+
+    Ok(())
+}
--- programs/solana_fpl/src/instructions/mod.rs
+++ programs/solana_fpl/src/instructions/mod.rs
@@ -1,3 +1,5 @@
 pub mod initialize_escrow;
+pub mod bet;
 
 pub use initialize_escrow::*;
+pub use bet::*;
