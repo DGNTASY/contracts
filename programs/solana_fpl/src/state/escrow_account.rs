cde1385434ef70d43557d4621c662e719f1cfb26
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-09-21 19:31:53 +0530

    refactor: modified escrow pda

--- programs/solana_fpl/src/state/escrow_account.rs
+++ programs/solana_fpl/src/state/escrow_account.rs
@@ -2,11 +2,11 @@ use anchor_lang::prelude::*;
 
 #[account]
 pub struct EscrowAccount {
-    pub authority: Pubkey,          
-    pub payout_first: u64,          
-    pub payout_second: u64,         
-    pub payout_third: u64,          
-    pub bet_amount: u128,           
-    pub usdc_balance: u64,          
-    pub bump: u8,   
+    pub authority: Pubkey,
+    pub usdc_mint: Pubkey,
+    pub payout_first: u64,
+    pub payout_second: u64,
+    pub payout_third: u64,
+    pub bet_amount: u128,
+    pub usdc_balance: u64,
 }
